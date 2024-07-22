5e07773cdb22e32991cfc10a92bdabcea71bae94
Timothy Japit <timothy.japit@gmail.com>
2024-07-22 15:04:44 +0700

    feat(Disassembler): mini-disassembler for our bytecode
    Instructions.String() so that we can print our bytecodes in a more
    human-friendly format.

--- src/code/code.go
+++ src/code/code.go
@@ -1,6 +1,7 @@
 package code
 
 import (
+	"bytes"
 	"encoding/binary"
 	"fmt"
 )
@@ -59,8 +60,44 @@ func Make(op Opcode, operands ...int) []byte {
 	return instruction
 }
 
-func (ins *Instructions) String() string {
-	return ""
+// Mini-disassembler
+func (ins Instructions) String() string {
+	var out bytes.Buffer
+
+	i := 0
+	for i < len(ins) {
+		def, err := Lookup(ins[i])
+		if err != nil {
+			fmt.Fprintf(&out, "ERROR: %s\n", err)
+			continue
+		}
+
+		operands, read := ReadOperands(def, ins[i+1:])
+
+		fmt.Fprintf(&out, "%04d %s\n", i, ins.fmtInstruction(def, operands))
+
+		i += 1 + read // the 1 is for the Opcode (i.e. Opcode + num of bytes read)
+	}
+
+	return out.String()
+}
+
+func (ins Instructions) fmtInstruction(def *Definiton, operands []int) string {
+	operandCount := len(def.OperandWidths)
+	if len(operands) != operandCount {
+		return fmt.Sprintf(
+			"ERROR: operand len %d does not match defined %d\n",
+			len(operands),
+			operandCount,
+		)
+	}
+
+	switch operandCount {
+	case 1:
+		return fmt.Sprintf("%s %d", def.Name, operands[0])
+	}
+
+	return fmt.Sprintf("ERROR: unhandled operandCount for %s\n", def.Name)
 }
 
 // Decodes bytecode made by Make(). Returns operands and number of bytes read.
