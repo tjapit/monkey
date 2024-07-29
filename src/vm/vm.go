889c3f9f532e9e44ebd969c9f9846525a332d813
Timothy Japit <timothy.japit@gmail.com>
2024-07-29 21:46:57 +0700

    feat(vm): constructor and "fetch" part of the cycle

--- src/vm/vm.go
+++ src/vm/vm.go
@@ -1,20 +1,43 @@
 package vm
 
 import (
+	"github.com/tjapit/monkey/src/code"
 	"github.com/tjapit/monkey/src/compiler"
 	"github.com/tjapit/monkey/src/object"
 )
 
-type VM struct{}
+const StackSize = 2048
 
-func New(bytecode *compiler.Bytecode) VM {
-	return VM{}
+type VM struct {
+	constants    []object.Object
+	instructions code.Instructions
+
+	stack []object.Object
+	sp    int // stackpointer: Always points to the next value. Top of stack is [sp-1]
+}
+
+func New(bytecode *compiler.Bytecode) *VM {
+	return &VM{
+		constants:    bytecode.Constants,
+		instructions: bytecode.Instructions,
+		stack:        make([]object.Object, StackSize),
+		sp:           0,
+	}
 }
 
 func (vm *VM) Run() error {
+	for ip := 0; ip < len(vm.instructions); ip++ {
+		op := code.Opcode(vm.instructions[ip])
+
+		switch op {
+		}
+	}
 	return nil
 }
 
 func (vm *VM) StackTop() object.Object {
-	return nil
+	if vm.sp == 0 {
+		return nil
+	}
+	return vm.stack[vm.sp-1]
 }
